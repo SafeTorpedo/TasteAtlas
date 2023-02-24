2333fa70d87ffad593c8bd6a7123261067d9dd19
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-02-25 00:55:05 +0530

    Added basic navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return (
-    <div className='h-screen bg-black text-white text-5xl text-center p-8'>
-      WELCOME TO TASTE ATLAS
-      {/* front end working */}
-    </div>
-  )
-}
+    return (
+        <React.Fragment>
+            <div className="h-screen bg-black text-white ">
+                <Navbar />
+                <h1 className="text-5xl text-center p-8">
+                    WELCOME TO TASTE ATLAS
+                </h1>
+                {/* front end working */}
+            </div>
+        </React.Fragment>
+    );
+};
 
-export default App
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="py-4 px-4 flex ">
+            TasteAtlas
+            <ul className="flex px-4 ml-52 ">
+                <li className="px-5">Log in</li>
+                <li>About us</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
