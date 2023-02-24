f0aba9a43b3795474cb3f2729e1e53dd78c11790
kennisjoseph <kennisjoseph5@gmail.com>
2023-02-24 23:21:28 +0530

    Schema done!

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./connection')
+const createRecipe = require('./createRecipe');
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,18 @@ app.use(express.json());
 db().then(()=>{console.log("connected to cloud")})
 .catch((err)=>{console.log(err)});
 
+
+
+//this will create new post
+app.post("/recipe",async(req,res)=>{
+    try {
+        const {title, country, description, link} = req.body;
+        const newRecipe = await createRecipe.create({title, country, description, link})
+        res.json(newRecipe)
+    } catch (err) {
+        console.log(err.message)
+    }
+})
 
 //We need to have one connection.js where we will connect to mongo atlas via mongoose
 
